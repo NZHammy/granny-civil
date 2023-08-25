963ce286e4f51d3591e9d2208d816e15771e641e
NZHammy <105731534+NZHammy@users.noreply.github.com>
2023-08-26 00:34:04 +1200

    Update index.js
    profanity works now . next is either setting up a external profanity api or make my own word list

--- index.js
+++ index.js
@@ -12,7 +12,26 @@ client.once('ready', () => {
     console.log('Bot is ready.');
 });
 
+// List of forbidden words
+const forbiddenWords = ['badword1', 'badword2', 'badword3'];
+
 client.on('messageCreate', async message => {
+    if (message.author.bot) return; // Ignore messages from bots
+
+    const content = message.content.toLowerCase();
+    const hasForbiddenWord = forbiddenWords.some(word => content.includes(word));
+
+    if (hasForbiddenWord) {
+        // Delete the message
+        message.delete();
+
+        // Send a warning to the user
+        message.channel.send("Oh dear, dearie! Please mind your language and remember, I'm just a sweet grandmother trying to keep things civil.");
+
+        // Optional: Log the moderation action
+        console.log(`${message.author.tag}'s message was removed for using inappropriate language.`);
+    }
+
     if (message.content === `${prefix}calmdown`) {
         try {
             await message.channel.send("Oh dear, dearie! Please mind your language and remember, I'm just a sweet grandmother trying to keep things civil.");
