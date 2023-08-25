03543a8e362644c6a72cc8fef7186e596ffd3882
NZHammy <105731534+NZHammy@users.noreply.github.com>
2023-08-25 23:36:36 +1200

    Update index.js

--- index.js
+++ index.js
@@ -12,17 +12,13 @@ client.once('ready', () => {
     console.log('Bot is ready.');
 });
 
-client.on('messageCreate', message => {
-    if (!message.content.startsWith(prefix)) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command === 'calmdown') {
-        const profanityList = ['badword1', 'badword2', 'badword3'];
-
-        if (args.some(word => profanityList.includes(word.toLowerCase()))) {
-            message.channel.send("Oh dear, dearie! Please mind your language and remember, I'm just a sweet grandmother trying to keep things civil.");
+client.on('messageCreate', async message => {
+    if (message.content === `${prefix}calmdown`) {
+        try {
+            await message.channel.send("Oh dear, dearie! Please mind your language and remember, I'm just a sweet grandmother trying to keep things civil.");
+            await message.delete(); // Delete the user's command message
+        } catch (error) {
+            console.error('Error:', error);
         }
     }
 });
